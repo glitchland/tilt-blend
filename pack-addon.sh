15924febfb5e51241f1af05f644b22706e4bc33e
Glitch <vravr@glitch.land>
2020-04-10 11:00:31 -0600

    Update pack-addon.sh

--- pack-addon.sh
+++ pack-addon.sh
@@ -8,4 +8,4 @@ cp tilt-brush-toolkit/Python/tiltbrush/tilted.py tiltblend/
 
 # create the addon archive (its just a simple zip)
 rm tiltblend.zip 2>/dev/null
-zip -r tiltblend.zip tiltblend/
+zip -r tiltblend.zip tiltblend -x "tiltblend/__pycache__/*"
