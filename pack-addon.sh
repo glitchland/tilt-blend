9d856cd19d83d8afd93823ac4b4cf3242906827e
Glitch <vravr@glitch.land>
2020-04-10 09:38:50 -0600

    Add a packing utility

--- pack-addon.sh
+++ pack-addon.sh
@@ -5,3 +5,6 @@ git submodule update --remote
 
 # copy dependency from submodule 
 cp tilt-brush-toolkit/Python/tiltbrush/tilted.py tiltblend/
+
+# create the addon archive (its just a simple zip)
+zip -r tiltblend.zip tiltblend/
