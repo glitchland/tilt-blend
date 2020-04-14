18691ae6026ab0ca545a9967f44bdcd534a1d09d
Glitch <vravr@glitch.land>
2020-04-13 21:10:28 -0600

    Update __init__.py

--- tiltblend/__init__.py
+++ tiltblend/__init__.py
@@ -16,7 +16,7 @@ bl_info = {
     "author" : "glitch",
     "description" : "Blender Tiltbrush Tools",
     "blender" : (2, 80, 0),
-    "version" : (0, 0, 1),
+    "version" : (0, 0, 2),
     "location" : "View3D",
     "warning" : "",
     "category" : "Generic"
